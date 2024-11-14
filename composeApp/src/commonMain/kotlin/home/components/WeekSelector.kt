225025c49da4b53a90d5ab2c740108e157df0b39
marinjuricev <mjuric00@fesb.hr>
2024-11-14 20:32:13 +0100

    feat: Play around with animations

--- composeApp/src/commonMain/kotlin/home/components/WeekSelector.kt
+++ composeApp/src/commonMain/kotlin/home/components/WeekSelector.kt
@@ -1,24 +1,27 @@
 package home.components
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.TweenSpec
+import androidx.compose.animation.core.animateInt
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.isTraceInProgress
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.fastForEach
 import home.model.DayInfo
 
@@ -32,26 +35,36 @@ fun WeekSelector(
         modifier = modifier.background(MaterialTheme.colorScheme.onPrimary),
     ) {
         days.fastForEach { day ->
+            val selectedTransition = updateTransition(day.isSelected)
+            val targetFontWeight by selectedTransition.animateInt { isSelected ->
+                if (isSelected) FontWeight.Bold.weight else FontWeight.Normal.weight
+            }
+            val fontSize by selectedTransition.animateInt { isSelected ->
+                if (isSelected) 18 else 14
+            }
+            val dayValueBackground by animateColorAsState(
+                targetValue = if (day.isSelected) MaterialTheme.colorScheme.secondary else Color.Transparent,
+                animationSpec = TweenSpec(250)
+            )
+
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .clickable { onDayClick(day) },
+                    .clickable { onDayClick(day) }
+                    .padding(vertical = 4.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
             ) {
-                Text(text = day.name)
-                Text(text = day.value)
-
-                val backgroundColor by animateColorAsState(
-                    targetValue = if (day.isSelected) MaterialTheme.colorScheme.secondary else Color.Transparent
+                Text(
+                    text = day.name,
+                    fontSize = fontSize.sp,
+                    fontWeight = FontWeight(targetFontWeight)
                 )
-
-                Box(
+                Text(
+                    text = day.value,
                     modifier = Modifier
-                        .padding(2.dp)
-                        .size(12.dp)
-                        .clip(CircleShape)
-                        .background(backgroundColor)
+                        .background(color = dayValueBackground)
+                        .padding(2.dp),
                 )
             }
         }
