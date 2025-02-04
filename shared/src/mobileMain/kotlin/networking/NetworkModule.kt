023371c9c4d08c0aa1510deca9949c4c2828e08c
marinjuricev <mjuric00@fesb.hr>
2025-02-04 21:31:33 +0100

    chore: Remove the temp code to dig into the signature

--- shared/src/mobileMain/kotlin/networking/NetworkModule.kt
+++ shared/src/mobileMain/kotlin/networking/NetworkModule.kt
@@ -1,17 +1,7 @@
 package networking
 
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
 import org.koin.dsl.module
 
 val clientNetworkModule = module {
     single { buildHttpClient() }
-    HttpClient {
-
-    }.get("htttps:").body<String>() { exception ->
-        when(exception) {
-
-        }
-    }
 }
