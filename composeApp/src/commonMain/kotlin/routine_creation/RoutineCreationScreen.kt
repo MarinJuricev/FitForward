b2b00e1997c1bfb33d4dbcff8ae484e8ead50911
marinjuricev <mjuric00@fesb.hr>
2025-04-01 20:24:14 +0200

    chore: Start prototyping the layout of the routine creation very much WIP

--- composeApp/src/commonMain/kotlin/routine_creation/RoutineCreationScreen.kt
+++ composeApp/src/commonMain/kotlin/routine_creation/RoutineCreationScreen.kt
@@ -2,17 +2,24 @@ package routine_creation
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
-import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.ArcMode
 import androidx.compose.animation.core.ExperimentalAnimationSpecApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import design.FitBodyMediumText
@@ -53,21 +60,40 @@ fun RoutineCreationScreen(
                     title = "Routine Creation"
                 )
             }
-        ) {
-            FitCard(
+        ) { paddingValues ->
+            Column(
                 modifier = Modifier
-                    .padding(it)
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-                    .fillMaxWidth()
-                    .sharedBounds(
-                        sharedContentState = rememberSharedContentState(key = ROUTINE_KEY),
-                        animatedVisibilityScope = animatedContentScope,
-                        placeHolderSize = SharedTransitionScope.PlaceHolderSize.animatedSize,
-                        resizeMode = SharedTransitionScope.ResizeMode.RemeasureToBounds,
-                        boundsTransform = arcTransform(ArcMode.ArcBelow),
-                    )
+                    .padding(paddingValues)
+                    .padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
-                Column {
+                RoutineCreationInfoCard {
+                    Icon(Icons.Rounded.Search, contentDescription = "Search Routines")
+                    FitBodyMediumText(
+                        modifier = Modifier,
+                        text = "Search Routines"
+                    )
+                }
+
+                RoutineCreationInfoCard {
+                    Icon(Icons.Rounded.Add, contentDescription = "Create a new Routines")
+                    FitBodyMediumText(
+                        modifier = Modifier,
+                        text = "Create a new Routines"
+                    )
+                }
+                FitCard(
+                    modifier = Modifier
+                        .padding(vertical = 8.dp)
+                        .fillMaxWidth()
+                        .sharedBounds(
+                            sharedContentState = rememberSharedContentState(key = ROUTINE_KEY),
+                            animatedVisibilityScope = animatedContentScope,
+                            placeHolderSize = SharedTransitionScope.PlaceHolderSize.animatedSize,
+                            resizeMode = SharedTransitionScope.ResizeMode.RemeasureToBounds,
+                            boundsTransform = arcTransform(ArcMode.ArcBelow),
+                        )
+                ) {
                     FitBodyMediumText(
                         modifier = Modifier.sharedElement(
                             sharedTransitionScope.rememberSharedContentState(key = ROUTINE_TEXT_KEY),
@@ -77,13 +103,32 @@ fun RoutineCreationScreen(
                         text = "Routines"
                     )
 
-                    LazyColumn {
-                        items(state.routines, key = { routine -> routine.id }) { routine ->
-                            FitBodyMediumText(routine.name)
-                        }
-                    }
                 }
             }
+            LazyColumn {
+                items(state.routines, key = { routine -> routine.id }) { routine ->
+                    FitBodyMediumText(routine.name)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RoutineCreationInfoCard(
+    modifier: Modifier = Modifier,
+    content: @Composable RowScope.() -> Unit
+) {
+    FitCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 2.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            content()
         }
     }
 }
